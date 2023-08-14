47978c59918d235aa11e17627c807967bc3df13d
TheEngineofDeath <theengineofdeath@gmail.com>
2023-08-14 23:53:43 +0500

    v 0.01

--- module/config.js
+++ module/config.js
@@ -1,6 +1,20 @@
 export const CP2075 = {};
 
 CP2075.damageTypes = {
-cut: "CP2075.damage.cut",
-PI: "CP2075.damage.PI"
-};
+    cut: "CP2075.damage.cut",
+    PI: "CP2075.damage.PI"
+    };
+
+/*CP2075.rangedWeapon = {
+/   damage: "CP2075.rangedWeapon.damage",
+/   damageType: "CP2075.rangedWeapon.damageType",
+    acc: "CP2075.rangedWeapon.acc",
+    wt: "CP2075.rangedWeapon.wt",
+    rof: "CP2075.rangedWeapon.rof",
+    shots: "CP2075.rangedWeapon.shots",
+    minST: "CP2075.rangedWeapon.minST",
+    bulk: "CP2075.rangedWeapon.bulk",
+    recoil: "CP2075.rangedWeapon.recoil",
+    lc: "CP2075.rangedWeapon.lc",
+    cost: "CP2075.rangedWeapon.cost" 
+    };*/
