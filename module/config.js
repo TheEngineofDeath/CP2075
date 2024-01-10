220d259e06e8cdd73bd8c7a196efcc6b83d48bf9
TheEngineofDeath <theengineofdeath@gmail.com>
2024-01-10 19:48:41 +0500

    Tables

--- module/config.js
+++ module/config.js
@@ -2,19 +2,21 @@ export const CP2075 = {};
 
 CP2075.damageType = {
     cut: "CP2075.damageType.cut",
-    PI: "CP2075.damageType.PI"
+    expansive: "CP2075.damageType.expansive",
+    bullet: "CP2075.damageType.bullet",
+    pierce: "CP2075.damageType.pierce"
     };
 
-/*CP2075.rangedWeapon = {
-/   damage: "CP2075.rangedWeapon.damage",
-/   damageType: "CP2075.rangedWeapon.damageType",
-    acc: "CP2075.rangedWeapon.acc",
-    wt: "CP2075.rangedWeapon.wt",
-    rof: "CP2075.rangedWeapon.rof",
-    shots: "CP2075.rangedWeapon.shots",
-    minST: "CP2075.rangedWeapon.minST",
-    bulk: "CP2075.rangedWeapon.bulk",
-    recoil: "CP2075.rangedWeapon.recoil",
-    lc: "CP2075.rangedWeapon.lc",
-    cost: "CP2075.rangedWeapon.cost" 
+/*CP2075.rWeapon = {
+/   damage: "CP2075.rWeapon.damage",
+/   damageType: "CP2075.rWeapon.damageType",
+    acc: "CP2075.rWeapon.acc",
+    wt: "CP2075.rWeapon.wt",
+    rof: "CP2075.rWeapon.rof",
+    shots: "CP2075.rWeapon.shots",
+    minST: "CP2075.rWeapon.minST",
+    bulk: "CP2075.rWeapon.bulk",
+    recoil: "CP2075.rWeapon.recoil",
+    lc: "CP2075.rWeapon.lc",
+    cost: "CP2075.rWeapon.cost" 
     };*/
